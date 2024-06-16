a9b4eb74518e0e3fb05d1c19c5bd789b5da83867
LiliyaAndreeva <122469561+LiliyaAndreeva@users.noreply.github.com>
2024-06-16 14:22:08 +0500

    Update build.sh
    change build file

--- .github/build.sh
+++ .github/build.sh
@@ -1,9 +1,9 @@
-curl -ls https://install.tuist.io | bash
+curl -Ls https://install.tuist.io | bash
 tuist install 3.42.2
 tuist fetch
 tuist generate
 xcodebuild clean -quiet
 xcodebuild build-for-testing\
-  -workspace 'mdEditor.xcworkspace'\
-  -scheme 'mdEditor'\
-  -destination 'platform=IOS Simulator, name=Iphone 14 Pro'
+    -workspace 'mdEditor.xcworkspace' \
+    -scheme 'mdEditor' \
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro'
